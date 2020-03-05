b8f4b11b676dada0d3a859094644e058f0d295fc
CalebVia <55600946+CalebVia@users.noreply.github.com>
2020-03-05 15:57:20 -0800

    Unbirthday

--- src/_02_unbirthday/Unbrithday.java
+++ src/_02_unbirthday/Unbrithday.java
@@ -1,5 +1,20 @@
 package _02_unbirthday;
 
+import javax.swing.JOptionPane;
+
 public class Unbrithday {
 
+public static void main(String[] args) {
+	
+
+String birthday = JOptionPane.showInputDialog("When is your birthday");
+
+if(birthday.equals("03/05/2020") {
+	showMessagedialog("Happy Birth")
+	
+	
 }
+
+}
+}
+
