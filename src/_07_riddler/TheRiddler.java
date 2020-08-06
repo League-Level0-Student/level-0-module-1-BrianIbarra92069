890f9892c1b222a35e158d511552fb0dd06dcf18
root <root@2e8412342b17>
2020-08-06 23:09:18 +0000

    k

--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -5,31 +5,37 @@ import javax.swing.JOptionPane;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
-
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-			int score = 0;
+		int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-			String riddler	=  JOptionPane.showInputDialog("Which word in the dictionary is spelled incorrectly?");
-			
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-			if (riddler.equals("Incorrectly")){
+		String riddler = JOptionPane.showInputDialog("Which word in the dictionary is spelled incorrectly?");
+
+		// 4. If they got the answer right, pop up "correct!" and increase the score by
+		// one
+		if (riddler.equals("Incorrectly")) {
 			JOptionPane.showMessageDialog(null, "Correct");
-			score++;   	}
-			if (riddler.equals("An Onion")) {
-			JOptionPane.showMeasageDialog()
+			score++;
+		}
 		// 5. Otherwise, say "wrong" and tell them the answer
-			else {  
-				JOptionPane.showMessageDialog(null, "Wrong!!");
-			}
+		else {
+			JOptionPane.showMessageDialog(null, "Wrong!!");
+		}
 		// 6. Add some more riddles
-riddler = JOptionPane.showInputDialog("Take off my skin - I won't cry, but you will! What am I?");
+		riddler = JOptionPane.showInputDialog("Take off my skin - I won't cry, but you will! What am I?");
 		// 2. Make a pop up to show the score.
-		  JOptionPane.showMessageDialog(null, score);
+		if (riddler.equals("An Onion")) {
+			JOptionPane.showMessageDialog(null, "Correct");
+			score++;
+		}
+
+		else {
+			JOptionPane.showMessageDialog(null, "Wrong!!");
+		}
+		
+		JOptionPane.showMessageDialog(null, score);
 	}
 }
-
